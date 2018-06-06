28d80ae40dae2d193c6aee88a53083e20137ac91
Geetika Batra <gbatra@Geetikas-MacBook-Pro.local>
2018-06-06 11:54:02 +0530

    Adds pre hooks to openshift temolate
    Signed-off-by: Geetika Batra gbatra@redhat.com

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Start API backbone service with time out
-gunicorn --pythonpath f8a_ingestion/ -b 0.0.0.0:$INGESTION_API_SERVICE_PORT -t $INGESTION_API_SERVICE_TIMEOUT rest_api:app
+gunicorn --pythonpath /f8a_ingestion/ -b 0.0.0.0:$INGESTION_API_SERVICE_PORT -t $INGESTION_API_SERVICE_TIMEOUT -k $CLASS_TYPE -w $NUMBER_WORKER_PROCESS rest_api:app
