0b722e2fc5e6844b876f5df370813bc903cd2be1
Michal Srb <michal@redhat.com>
2018-06-19 13:19:49 +0200

    Don't immediately freak out if virtualenv command is not available

--- runtests.sh
+++ runtests.sh
@@ -34,8 +34,8 @@ gc() {
 trap gc EXIT SIGINT
 
 function prepare_venv() {
-    VIRTUALENV=`which virtualenv`
-    if [ $? -eq 1 ]
+    VIRTUALENV=`which virtualenv` || :
+    if [ -z "$VIRTUALENV" ]
     then
         # python34 which is in CentOS does not have virtualenv binary
         VIRTUALENV=`which virtualenv-3`
