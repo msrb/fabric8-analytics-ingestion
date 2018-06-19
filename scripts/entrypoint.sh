0f83f991380a8a97bf91820cd86f057d2f07d50a
Michal Srb <michal@redhat.com>
2018-06-19 21:05:48 +0200

    Rework entrypoint.sh script a bit

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# Start API ingestion service with time out
-gunicorn --pythonpath /f8a_ingestion/ -b 0.0.0.0:$INGESTION_API_SERVICE_PORT -t $INGESTION_API_SERVICE_TIMEOUT -k $CLASS_TYPE -w $NUMBER_WORKER_PROCESS rest_api:app
+set -e
+
+DEFAULT_GUNICORN_ARGS="--worker-class sync --workers 4"
+
+if [ -z "$GUNICORN_ARGS" ]; then
+    echo "Using default gunicorn args: ${DEFAULT_GUNICORN_ARGS}"
+    GUNICORN_ARGS=${DEFAULT_GUNICORN_ARGS}
+fi
+
+set -x
+# Start API ingestion service
+gunicorn --pythonpath /f8a_ingestion/ \
+         --bind 0.0.0.0:${APP_PORT:-5000} \
+         ${GUNICORN_ARGS} \
+         rest_api:app
