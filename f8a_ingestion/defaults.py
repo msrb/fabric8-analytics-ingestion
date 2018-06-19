5c9975595ac94606e73fc020056eae22af145d0b
Michal Srb <michal@redhat.com>
2018-06-19 17:44:08 +0200

    WIP

--- f8a_ingestion/defaults.py
+++ f8a_ingestion/defaults.py
@@ -17,44 +17,54 @@
 
 """Configuration."""
 
+import os
 import logging
-from urllib.parse import quote, urljoin
-
-import random
-from os import environ, path
-
-from f8a_ingestion.errors import F8AConfigurationException
+from urllib.parse import quote
 
 logger = logging.getLogger(__name__)
 
-SWAGGER_YAML_PATH = path.join(path.dirname(path.realpath(__file__)), '../swagger/swagger.yaml')
+SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../swagger/swagger.yaml')
+
+DEFAULT_PGDATABASE = 'f8a-ingestion'
+DEFAULT_PGPORT = '5432'
 
 
-class F8AConfiguration(object):
-    """Configuration."""
+def get_psql_conn_str(enable_ssl=False):
 
-    def _make_postgres_string(password):
-        """Create postgres connection string.
+    conn_str = 'postgresql://{user}:{password}@{host}:{port}/{db}?sslmode={ssl}'.format(
+        user=os.environ.get('PGUSER'),
+        password=os.environ.get('PGPASSWORD'),
+        host=os.environ.get('PGHOST'),
+        port=os.environ.get('PGPORT', DEFAULT_PGPORT),
+        db=os.environ.get('PGDATABASE', DEFAULT_PGDATABASE),
+        ssl=str(enable_ssl).lower()
+    )
 
-        It's parametrized, so it's possible to
-        create either quoted or unquoted version of connection string.
-        Note that it's outside of class since there is no simple way how to call it inside the class
-        without class initialization.
-        :param password: password which will be embedded into Postgres connection string
-        :return: fully working postgres connection string
-        """
-        connection = 'postgresql://{user}:{password}@{pgbouncer_host}:{pgbouncer_port}' \
-                     '/{database}?sslmode=disable'. \
-            format(user=environ.get('POSTGRESQL_USER'),
-                   password=environ.get('POSTGRESQL_PASSWORD'),
-                   pgbouncer_host=environ.get('PGBOUNCER_SERVICE_HOST', 'coreapi-postgres'),
-                   pgbouncer_port=environ.get('PGBOUNCER_SERVICE_PORT', '5432'),
-                   database=environ.get('POSTGRESQL_DATABASE'))
-        return connection
-
-    UNQUOTED_POSTGRES_CONNECTION = _make_postgres_string(environ.get('POSTGRESQL_PASSWORD', ''))
-    POSTGRES_CONNECTION = _make_postgres_string(
-        quote(environ.get('POSTGRESQL_PASSWORD', ''), safe=''))
+    return conn_str
 
 
-configuration = F8AConfiguration()
+#     def _make_postgres_string(password):
+#         """Create postgres connection string.
+#
+#         It's parametrized, so it's possible to
+#         create either quoted or unquoted version of connection string.
+#         Note that it's outside of class since there is no simple way how to call it inside the class
+#         without class initialization.
+#         :param password: password which will be embedded into Postgres connection string
+#         :return: fully working postgres connection string
+#         """
+#         connection = 'postgresql://{user}:{password}@{pgbouncer_host}:{pgbouncer_port}' \
+#                      '/{database}?sslmode=disable'. \
+#             format(user=environ.get('POSTGRESQL_USER'),
+#                    password=environ.get('POSTGRESQL_PASSWORD'),
+#                    pgbouncer_host=environ.get('PGBOUNCER_SERVICE_HOST', 'coreapi-postgres'),
+#                    pgbouncer_port=environ.get('PGBOUNCER_SERVICE_PORT', '5432'),
+#                    database=environ.get('POSTGRESQL_DATABASE'))
+#         return connection
+#
+#     UNQUOTED_POSTGRES_CONNECTION = _make_postgres_string(environ.get('POSTGRESQL_PASSWORD', ''))
+#     POSTGRES_CONNECTION = _make_postgres_string(
+#         quote(environ.get('POSTGRESQL_PASSWORD', ''), safe=''))
+#
+#
+# configuration = F8AConfiguration()
