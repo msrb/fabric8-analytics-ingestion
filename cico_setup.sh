c2613050e905acdd2b5ce675931205d38f11be3b
Michal Srb <michal@redhat.com>
2018-06-19 09:22:03 +0200

    Fix cico scripts

--- cico_setup.sh
+++ cico_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+REGISTRY="push.registry.devshift.net"
+
 load_jenkins_vars() {
     if [ -e "jenkins-env" ]; then
         cat jenkins-env \
@@ -10,6 +12,15 @@ load_jenkins_vars() {
     fi
 }
 
+docker_login() {
+    if [ -n "${DEVSHIFT_USERNAME}" -a -n "${DEVSHIFT_PASSWORD}" ]; then
+        docker login -u ${DEVSHIFT_USERNAME} -p ${DEVSHIFT_PASSWORD} ${REGISTRY}
+    else
+        echo "Could not login, missing credentials for the registry"
+        exit 1
+    fi
+}
+
 prep() {
     yum -y update
     yum -y install git which epel-release
@@ -17,6 +28,45 @@ prep() {
     yum -y install docker
     systemctl start docker
     pip3 install virtualenv
+}
+
+build_image() {
+    make docker-build
+}
+
+tag_push() {
+    local target=$1
+    local source=$2
+    docker tag ${source} ${target}
+    docker push ${target}
+}
+
+push_image() {
+    local image_name
+    local image_repository
+    local short_commit
+    image_name=$(make get-image-name)
+    image_repository=$(make get-image-repository)
+    short_commit=$(git rev-parse --short=7 HEAD)
+
+    if [ "$TARGET" = "rhel" ]; then
+        IMAGE_URL="${REGISTRY}/osio-prod/${image_repository}"
+    else
+        IMAGE_URL="${REGISTRY}/${image_repository}"
+    fi
+
+    if [ -n "${ghprbPullId}" ]; then
+        # PR build
+        pr_id="SNAPSHOT-PR-${ghprbPullId}"
+        tag_push ${IMAGE_URL}:${pr_id} ${image_name}
+        tag_push ${IMAGE_URL}:${pr_id}-${short_commit} ${image_name}
+    else
+        # master branch build
+        tag_push ${IMAGE_URL}:latest ${image_name}
+        tag_push ${IMAGE_URL}:${short_commit} ${image_name}
+    fi
+
+    echo 'CICO: Image pushed, ready to update deployed app'
 }
 
 load_jenkins_vars
